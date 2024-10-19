2d791f1449c3aeb2601291688271dc808cffb0c9
Alaoefyn <121045925+Alaoefyn@users.noreply.github.com>
2024-10-20 01:18:43 +0300

    Create ubuntu_freshset.sh

---
+++ ubuntu_freshset.sh
@@ -0,0 +1,131 @@
+bash_script_content = """
+#!/bin/bash
+
+# Extend ZRAM and Swap Space
+
+echo "Extending ZRAM and Swap space..."
+sudo apt install zram-tools -y
+sudo sed -i 's/PERCENTAGE=25/PERCENTAGE=400/' /etc/default/zramswap
+sudo swapoff -a
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+# Install zsh
+
+sudo apt update
+sudo apt install zsh -y
+# chsh -s $(which zsh)  (uncomment if you want to make zsh default)
+
+# KVM/QEMU Installation and Setup
+
+echo "Installing KVM/QEMU and setting up virtual machine directories..."
+sudo apt update
+sudo apt install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
+sudo usermod -aG libvirt,kvm $(whoami)
+
+
+# Moving KVM storage to another disk
+
+sudo systemctl stop libvirtd
+sudo mkdir -p /mnt/storage/kvm
+sudo mv /var/lib/libvirt/images /mnt/storage/kvm/
+sudo ln -s /mnt/storage/kvm/images /var/lib/libvirt/images
+sudo systemctl start libvirtd
+sudo virsh pool-define-as default dir - - - - "/mnt/storage/kvm/images"
+sudo virsh pool-build default
+sudo virsh pool-start default
+sudo virsh pool-autostart default
+
+# Flatpak Installation
+echo "Installing Flatpak and adding Flathub repository..."
+sudo apt install flatpak -y
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Installing Flatseal
+echo "Installing Flatseal..."
+flatpak install flathub com.github.tchx84.Flatseal -y
+
+# VSCode Installation via Flatpak and Extension Installation
+
+echo "Installing Visual Studio Code via Flatpak and adding common extensions..."
+flatpak install flathub com.visualstudio.code -y
+flatpak run com.visualstudio.code --install-extension ms-python.python
+flatpak run com.visualstudio.code --install-extension Dart-Code.flutter
+flatpak run com.visualstudio.code --install-extension ms-vscode.cpptools
+flatpak run com.visualstudio.code --install-extension eamodio.gitlens
+
+# Removing unnecessary background applications (GNOME Editions)
+
+
+ps aux | grep evolution
+#Check if evolution is there, it is using too much ram on background, for me it is useless.
+
+systemctl --user stop evolution-addressbook-factory.service
+systemctl --user stop evolution-calendar-factory.service
+systemctl --user stop evolution-source-registry.service
+systemctl --user stop evolution-data-server.service
+
+systemctl --user disable evolution-addressbook-factory.service
+systemctl --user disable evolution-calendar-factory.service
+systemctl --user disable evolution-source-registry.service
+systemctl --user disable evolution-data-server.service
+
+sudo apt remove evolution evolution-data-server -y
+sudo apt autoremove -y
+
+# Remove Remmina Desktop Client
+sudo apt remove remmina -y
+
+# Remove Transmission (BitTorrent client)
+
+sudo apt remove transmission-gtk -y
+
+# Remove Downloaded Games
+
+sudo apt remove aisleriot gnome-mahjongg gnome-sudoku -y
+
+# Remove Thunderbird (Mail Client)
+
+sudo apt remove thunderbird -y
+
+#Remove Snap Store (I use mostly flatpak, you can stick to the snap)
+sudo snap remove snap-store
+
+# Remove Amazon Web Launcher 
+sudo apt remove ubuntu-web-launchers
+
+# Steam Flatpak Installation
+
+echo "Installing Steam via Flatpak..."
+flatpak install flathub com.valvesoftware.Steam -y
+# Steam Library should be moved manually using Steam interface
+
+# Installing Software Development Tools (Flutter, Python, etc.)
+
+echo "Installing development tools (Flutter, Python, MySQL, PostgreSQL, Node.js, Java)..."
+sudo snap install flutter --classic
+sudo apt install python3 python3-pip -y
+sudo apt install mysql-server postgresql -y
+sudo apt install nodejs npm -y
+sudo apt install openjdk-11-jdk -y
+
+# Telegram Flatpak Installation
+echo "Installing Telegram via Flatpak..."
+flatpak install flathub org.telegram.desktop -y
+
+
+# Final Touch
+sudo apt autoremove
+
+echo "Config completed."
+"""
+
+# Save the script to a file
+file_path = "/mnt/data/ubuntu_freshset.sh"
+with open(file_path, "w") as file:
+    file.write(bash_script_content)
+
+file_path
